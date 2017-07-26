82bcc9a9bf178a6c8a7355247d8ab3901a4d15f4
Tanner <tannerjjones@gmail.com>
2017-07-26 18:58:27 +0000

    added tests for no match

--- test/getAbbreviation-test.js
+++ test/getAbbreviation-test.js
@@ -5,6 +5,11 @@ const assert = require('chai').assert;
 const getAbbreviation = require('../lib/main.js').getAbbreviation
 
 it('should get the correct abbreviations, or full length words for the matching strings', ()=>{
-    console.log('moomoo')
     assert.deepEqual(getAbbreviation('Street'), 'St')
+    assert.deepEqual(getAbbreviation('St'), 'Street')
+})
+
+it('should return the word if there is no abbreviation', ()=>{
+    assert.deepEqual(getAbbreviation('Main'), 'Main')
+    assert.deepEqual(getAbbreviation('123'), '123')
 })
