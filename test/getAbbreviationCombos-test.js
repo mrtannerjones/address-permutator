4fe5c034c5a4e72b8c85e981e51c8349050caf8d
Tanner <tannerjjones@gmail.com>
2017-07-26 19:04:44 +0000

    edited test naming for combos and simplified function

--- test/getAbbreviationCombos-test.js
+++ test/getAbbreviationCombos-test.js
@@ -4,7 +4,7 @@
 const assert = require('chai').assert;
 const getAbbreviationCombos = require('../lib/main.js').getAbbreviationCombos
 
-it('should get the correct abbreviations, or full length words for the matching strings', ()=>{
+it('make an array for each word in the string, including the original and any abbreviations or lengthenings', ()=>{
     const expected = [['123'], ['North', 'N'], ['Main'], ['St', 'Street']]
     assert.deepEqual(getAbbreviationCombos('123 North Main St'), expected)
 })
