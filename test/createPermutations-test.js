7e0bc1cd4d02c33fc23b4cde545ed0559d2e3c26
Tanner <tannerjjones@gmail.com>
2017-07-26 19:09:02 +0000

    successful test for create permutations

--- test/createPermutations-test.js
+++ test/createPermutations-test.js
@@ -6,5 +6,11 @@ const createPermutations = require('../lib/main.js').createPermutations
 
 it('make an array for each word in the string, including the original and any abbreviations or lengthenings', ()=>{
     const arrayOfWordPairs = [['123'], ['North', 'N'], ['Main'], ['St', 'Street']]
+    const expected = [
+        "123 North Main St",
+        "123 N Main St",
+        "123 North Main Street",
+        "123 N Main Street"
+    ]
     assert.deepEqual(createPermutations(arrayOfWordPairs), expected)
 })
